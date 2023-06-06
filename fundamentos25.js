6b24f765166fa77ec315234962103de6d366340a
Lucas Coutinho <cucasloutinho@gmail.com>
2023-06-05 23:39:22 -0300

    commit

---
+++ fundamentos25.js
@@ -0,0 +1,10 @@
+/*
+25) Escrever um programa para exibir os números de 1 até 50 na tela.
+*/
+function exibirNumeros (){
+    for(i = 1; i <= 50; i++){
+        console.log("número: ",[i])
+    }
+    
+    }
+    exibirNumeros()
