a9405bdb422c6f8403e4d20625f93c7ab222d878
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-06-10 02:38:10 -0300

    Conteúdo aula
    Conteúdo aula

---
+++ Fundamentos/node/moduloA.js
@@ -0,0 +1,6 @@
+
+//console.log(this)
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Ate o proximo exemplo'//exporta os ola, bemVindo e ateLogo
---
+++ Fundamentos/node/moduloB.js
@@ -0,0 +1,8 @@
+
+// foma amis comum de exportar
+module.exports = {
+    bomDia: 'Bom dia', 
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
