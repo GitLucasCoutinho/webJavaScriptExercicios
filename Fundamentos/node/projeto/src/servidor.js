32b624171cae2ca923ca8f0435955a4d78343518
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-06-13 22:54:25 -0300

    Conteúdo aula
    Conteúdo aula

---
+++ Fundamentos/node/projeto/src/servidor.js
@@ -0,0 +1,50 @@
+//teste para executar o que esta sendo executado node src/servidor.js no terminal 
+//npm i --save body-parser
+// node start
+
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+/*/
+app.get('/produtos', (req, res, next) => {
+    console.log('Middlewere 1...')
+    next()
+})
+
+//mocado
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'Notebook', preco: 123.45})
+})
+
+app.listen(porta, () =>{
+    console.log(`Servidor executando na porta ${porta}.`)
+})
+*/
+
+app.get("/produtos", (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //no final das contas aqui vai converter para JSON 
+})
+
+app.listen(porta, () => {
+    console.log(`servidor esta executando na porta ${porta}.`)
+})
+
+
