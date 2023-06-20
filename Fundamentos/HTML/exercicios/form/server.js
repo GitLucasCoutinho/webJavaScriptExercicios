f2826b7284b36ec8a81de2b2562cc514ddc3cb63
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-06-20 02:15:51 -0300

    Conteúdo aula
    Conteúdo aula

--- Fundamentos/HTML/exercicios/form/server.js
+++ Fundamentos/HTML/exercicios/form/server.js
@@ -1,3 +1,5 @@
+//npm i --save-dedv nodemon
+//nodemon server.js
 const express = require('express')
 const app = express()
 //Uma vez que eu submeto os dados ddo formulario ele vai pegar os input do corpo da requisicao eincluir no request.body
@@ -10,7 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true}))
 //vai atender uma requisicao do tipo post no caso o /usuarios
 app.post('/usuarios', (req, resp) => { 
     console.log(req.body)
-    resp.send('<h1>Parabens</h1>')
+    resp.send('<h1>Parabens. Usuario incluido!!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => { 
+    console.log(req.body)
+    console.log(req.params.id)
+    resp.send('<h1>Parabens. Usuario alterado!!! </h1>')
 })
 
 app.listen(3003)
