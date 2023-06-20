64939c112c25498f3a606ebac3e6e38cf18f2d38
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-06-19 23:13:42 -0300

    Conteúdo aula
    Conteúdo aula

--- Fundamentos/HTML/exercicios/form/server.js
+++ Fundamentos/HTML/exercicios/form/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+//Uma vez que eu submeto os dados ddo formulario ele vai pegar os input do corpo da requisicao eincluir no request.body
 const bodyParser = require('body-parser')
 
-
+//urlencoded eh o padrao 
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.post('/usuarios', (req, resp) => {
+
+//vai atender uma requisicao do tipo post no caso o /usuarios
+app.post('/usuarios', (req, resp) => { 
     console.log(req.body)
     resp.send('<h1>Parabens</h1>')
 })
