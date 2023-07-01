495f6e3f0eb20c6947b10d8eccbb1aa461690313
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-07-01 12:31:02 -0300

    Conteúdo aula
    Conteúdo aula

--- Fundamentos/react/calculadora/src/components/Button.jsx
+++ Fundamentos/react/calculadora/src/components/Button.jsx
@@ -1,5 +1,20 @@
-import react from 'react'
+import React from 'react'
 import './Button.css'
 
-export default props => 
-<button calssName='button'>0</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+
+
+
+    return(
+    <button 
+        onClick={ e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label}
+        </button>
+    )
+}
