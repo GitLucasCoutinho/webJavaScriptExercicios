114d64551a370cd3d2d600c505d2b7b4d59231d5
Lucas Coutinho <CucasLoutinho@gmail.com>
2023-07-12 21:41:42 -0300

    Conteúdo aula
    Conteúdo aula

--- Fundamentos/vue/calculator/src/main/Calculator.vue
+++ Fundamentos/vue/calculator/src/main/Calculator.vue
@@ -50,9 +50,17 @@ export default {
                 const currentOperation = this.operation
 
                 try {
+                   
                     this.values[0] = eval(
                         `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                        
                     )
+                    if (isNaN(this.values[0]) || !isFinite(this.values[0])) {
+                        this.clearMemory()
+                        return
+                    }
+
+
                 } catch (e) {
                     this.$emit('onError', e)
                 }
